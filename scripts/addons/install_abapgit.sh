4c6ed49b82f95a38694483b5b519f617ae17f127
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2019-04-12 00:34:37 +0300

    install abapgit script

---
+++ scripts/addons/install_abapgit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $(id -u) = '0' ]; then
+    echo "[!] install_abapgit script must not be run from root"
+    exit 1
+fi
+
+node -v > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "[!] Install nodejs before running this script"
+    exit 1
+fi
+
+echo "Preparing to install abapgit ..."
+cp -R /vagrant/abapdeploy.js ~/
+cd ~/abapdeploy.js
+npm -q install
+
+echo "Downloading latest abapgit source ..."
+curl -sS https://raw.githubusercontent.com/abapGit/build/master/zabapgit.abap -o /tmp/zabapgit.abap
+
+echo "Installing abapgit ..."
+export AD_IP=localhost
+export AD_USER=developer
+export AD_PASS=Down1oad
+node abapdeploy.js -f /tmp/zabapgit.abap -p zabapgit
