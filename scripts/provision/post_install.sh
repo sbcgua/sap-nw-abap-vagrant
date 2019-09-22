0361337cc1f26c2c1a42468d8aa6d5f91246c2df
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2019-09-22 17:43:39 +0300

    increase workers no

--- scripts/provision/post_install.sh
+++ scripts/provision/post_install.sh
@@ -2,6 +2,12 @@
 
 # sh /vagrant/scripts/install_addons.sh
 
+echo "Adjusting SAP profile - increasing workers number..."
+PROFILE_PATH=/sapmnt/NPL/profile/NPL_D00_vhcalnplci
+sudo cp $PROFILE_PATH $PROFILE_PATH.bak
+sudo sed -i -e 's/wp_no_dia = 2/wp_no_dia = 6/' $PROFILE_PATH
+
+echo "SAP HARDWARE KEY"
 sudo -i -u npladm saplicense -get || true
 
 echo "Installation sequence complete"
