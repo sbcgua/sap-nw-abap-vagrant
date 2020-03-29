f2bc2238d0a3c07758775d6010a2049170b985af
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-03-29 20:32:20 +0300

    client_sni_enabled to profile

--- scripts/provision/post_install.sh
+++ scripts/provision/post_install.sh
@@ -5,5 +5,9 @@ PROFILE_PATH=/sapmnt/NPL/profile/NPL_D00_vhcalnplci
 sudo cp $PROFILE_PATH $PROFILE_PATH.bak
 sudo sed -i -e 's/wp_no_dia = 2/wp_no_dia = 6/' $PROFILE_PATH
 
+#ssl/ciphersuites         =  135:PFS:HIGH::EC_P256:EC_HIGH
+#ssl/client_ciphersuites  =  150:PFS:HIGH::EC_P256:EC_HIGH
+echo "ssl/client_sni_enabled = TRUE" >> $PROFILE_PATH
+
 echo "SAP HARDWARE KEY"
 sudo -i -u npladm saplicense -get || true
