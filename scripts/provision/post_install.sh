f2ff6ea69dc504adef509e11f3f044755d11d351
Alexander Tsybulsky <sbcgua@users.noreply.github.com>
2021-07-08 07:55:16 +0300

    update post_install, sni_enabled to default.pfl

--- scripts/provision/post_install.sh
+++ scripts/provision/post_install.sh
@@ -5,9 +5,10 @@ PROFILE_PATH=/sapmnt/NPL/profile/NPL_D00_vhcalnplci
 sudo cp $PROFILE_PATH $PROFILE_PATH.bak
 sudo sed -i -e 's/wp_no_dia = 2/wp_no_dia = 6/' $PROFILE_PATH
 
+DEFAULT_PROFILE_PATH=/sapmnt/NPL/profile/DEFAULT.PFL
 #ssl/ciphersuites         =  135:PFS:HIGH::EC_P256:EC_HIGH
 #ssl/client_ciphersuites  =  150:PFS:HIGH::EC_P256:EC_HIGH
-echo "ssl/client_sni_enabled = TRUE" >> $PROFILE_PATH
+echo "ssl/client_sni_enabled = TRUE" >> $DEFAULT_PROFILE_PATH
 
 echo "SAP HARDWARE KEY"
 sudo -i -u npladm saplicense -get || true
